8d82b6d4d616f6a61f9666cf640dad07e8884290
Austin Nevins <amnevins@gmail.com>
2018-02-10 14:18:45 -0800

    Add Lambda For Getting Developer Profiles

---
+++ RTW/GetDeveloperProfiles.js
@@ -0,0 +1,36 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	console.log(event);
+	const success = {
+		"statusCode": 200,
+		"headers": {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/json"
+		},
+		"body": "..."
+	}
+	const failure = {
+		"statusCode": 500,
+		"headers": {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "text/plain"
+		},
+		"body": "Shit broke."
+	}
+	ddb.scan({
+		TableName: 'developers'
+	}, function (err, data) {
+		if (err) {
+			console.log(err);
+			return callback(null, failure);
+		} else {
+			console.log('it fuckin worked!')
+			success.body = JSON.stringify(data);
+			return callback(null, success);
+		}
+	});
+
+
+	callback(null, 'Successfully executed');
+}
